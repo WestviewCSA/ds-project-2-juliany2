9e7c3d21b5d3b83da2669cd3ede015cb3d6eac9f
1918486 <1918486@01-18105.pusd.dom>
2025-03-11 14:06:10 -0700

    reading in maps

---
+++ P2_first_last/src/Map.java
@@ -0,0 +1,4 @@
+
+public class Map {
+	private Tile[][] map;
+}
--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		
+		readMap("test1.txt");
 	}
-
+	
+	public static void readMap(String filename) {
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			
+			// process the map
+			while (scanner.hasNextLine()) {
+				// grab a line (one row)
+				String row = scanner.nextLine();
+				
+				if (row.length() > 0) {
+					for (int i = 0; i < numCols && i < row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, el);
+					}
+				}
+				
+				
+				rowIndex++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+		
+	}
 }
