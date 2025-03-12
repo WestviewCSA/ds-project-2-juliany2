aaa74ff95b7d139b0a77c8267db62b2838420010
1918486 <1918486@01-18105.pusd.dom>
2025-03-12 13:50:19 -0700

    finished reading in map and started coordinate input

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -1,4 +1,12 @@
 
 public class Map {
 	private Tile[][] map;
+
+	public Map(int r, int c) {
+		map = new Tile[r][c];
+	}
+	
+	public void set(int r, int c, Tile tile) {
+		map[r][c] = tile;
+	}
 }
--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class p2 {
+	
+	static Map grid;
 
 
 	public static void main(String[] args) {
@@ -20,10 +22,14 @@ public class p2 {
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
 			
+			grid = new Map(numRows, numCols);
+
 			int rowIndex = 0;
 			
+			Tile[][] input = new Tile[numRows][numCols];
+			
 			// process the map
-			while (scanner.hasNextLine()) {
+			while (scanner.hasNextLine() && rowIndex < numRows) {
 				// grab a line (one row)
 				String row = scanner.nextLine();
 				
@@ -31,16 +37,28 @@ public class p2 {
 					for (int i = 0; i < numCols && i < row.length(); i++) {
 						char el = row.charAt(i);
 						Tile obj = new Tile(rowIndex, i, el);
+						input[rowIndex][i] = new Tile(rowIndex, i, el);
+						grid.set(rowIndex, i, obj);
 					}
 				}
 				
 				
 				rowIndex++;
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
-		
+	}
+	
+	public static void readCoordinates(String filename) {
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 	}
 }
